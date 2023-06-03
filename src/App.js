cd76b3124f7bceb54ce92f0ab2f35183ad391a54
rezaazimibar <www.beta04@gmail.com>
2023-06-03 17:30:37 +0430

    add useRef hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import UseRealExample from "./hooks/UseRealExample";
+import { FaRocket } from "react-icons/fa";
 
 const App = () => {
   return (
     <>
       <header className=" text-center py-5 mx-48 border-b-2">
-        <h1>بارگیری و استفاده از هوک های ریکت</h1>
+        <h1 className=" flex justify-center"><FaRocket/>بارگیری و استفاده از هوک های ریکت </h1>
       </header>
       <main className="text-center my-10">
 
--- src/hooks/UseRealExample.jsx
+++ src/hooks/UseRealExample.jsx
@@ -1,19 +1,68 @@
+import { useRef, useEffect, useState } from "react";
+import { FaEye } from "react-icons/fa";
 const UseRealExample = () => {
+  const inputRef = useRef(null);
+
+  const [name, setName] = useState("");
+
+  // const [renderCont, setRenderCont] = useState(1);
+  const renderCount=useRef(1)
+
+  const prevName = useRef("");
+
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
+
+  useEffect(() => {
+    renderCount.current=renderCount.current+1
+  });
+
+
+  const focusInput = () => {
+    inputRef.current.placeholder = "این یک اسم رندوم است";
+  };
+
+
   return (
     <>
-      <main >
-      <div
-          class="bg-teal-100 border-t-4 w-1/3 m-auto border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+      <main>
+        <div
+          className="bg-teal-100 border-t-4 w-1/3 m-auto border-teal-500 transition rounded-b text-teal-900 hover:shadow-none hover:translate-y-2 px-4 py-3 shadow-md"
           role="alert"
         >
-          <div class="flex justify-center w">
+          <div className="flex justify-center w">
             <div>
-              <p class="font-bold">useRef آشنایی با هوک</p>
+              <p className="font-bold">useRef آشنایی با هوک</p>
             </div>
           </div>
         </div>
-        <input type="text" dir="rtl" className="input m-auto my-5" placeholder="اسم وارد کیند"></input>
-        <button className="btn w-1/3" >تمرکز کن</button>
+        <p dir="rtl">{`میزان رندر های انجام شده:${renderCount.current}`}</p>
+        <p dir="rtl">{`نام شما برابر است با :${name}`}</p>
+        <p dir="rtl">{`نام شما برابر است با :${prevName.current}`}</p>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          ref={inputRef}
+          type="text"
+          dir="rtl"
+          className="input m-auto my-5 "
+          placeholder="اسم وارد کیند"
+        />
+        <button
+          onClick={focusInput}
+          className="btn w-1/3 m-auto flex justify-center"
+        >
+          <FaEye />
+          تمرکز کن
+        </button>
       </main>
     </>
   );
